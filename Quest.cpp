0f04a86892b8ed44252e2ba39f1b223d87b92a28
Prodigg <raphael@familie-sauer.ch>
2024-03-27 21:34:59 +0100

    update

--- Quest.cpp
+++ Quest.cpp
@@ -1,3 +1,77 @@
-#include "Quest.h"
+﻿#include "Quest.h"
+//TODO: Quest system Testen und vertigstellen
 
 
+Quest::Quest(std::vector<QuestObjective> QuestObjectivs) : 
+QuestObjectivs(QuestObjectivs){
+	int* tmpArray = new int[QuestObjectivs.size() + 2];
+	tmpArray[0] = TEXT_HEAD;
+	for (size_t i = 0; i < QuestObjectivs.size(); i++) {
+		tmpArray[i + 1] = TEXT_BODY;
+	}
+	tmpArray[QuestObjectivs.size() + 2] = TEXT_END;
+	QuestDisplayTextBox->reformate(tmpArray, QuestObjectivs.size() + 2);
+	delete[] tmpArray;
+}
+
+void Quest::drawQuest(int x, int y, Console* console) {
+	QuestDisplayTextBox->clearText();
+	// find longest string
+	int longestString = 0;
+	for (size_t i = 0; i < QuestObjectivs.size(); i++) {
+		if (longestString < QuestObjectivs.at(i).Description.length()) longestString = QuestObjectivs.at(i).Description.length();
+	}
+	
+	// construct Quest Display Strings
+	for (size_t i = 0; i < QuestObjectivs.size(); i++) {
+		std::wstring TmpString = L" ";
+		// check completed status
+		if (QuestObjectivs.at(i).completed == true) {
+			TmpString += L"█"; // completed
+		}
+		else {
+			TmpString += L"░"; // not completed
+		}
+		TmpString += L" | ";
+		TmpString += QuestObjectivs.at(i).Description;
+
+		QuestDisplayTextBox->setText(i + 1, 1, TmpString);
+
+	}
+	QuestDisplayTextBox->display(x, y, console);
+	return;	
+}
+
+void Quest::setQuestObjectiveStatus(int QuestObjectiveID, bool status) {
+	// search for QuestObjectiveID
+	int QuestObjectivePos = -1;
+	for (size_t i = 0; i < QuestObjectivs.size(); i++) {
+		if (QuestObjectivs.at(i).QuestObjectiveID == QuestObjectiveID) {
+			QuestObjectivePos = i;
+			break;
+		}
+	}
+
+	// return if no QuestQobjective was found
+	if (QuestObjectivePos == -1) return;
+
+	// set QuestObjective to status
+	QuestObjectivs.at(QuestObjectivePos).completed = status;
+	return;
+}
+
+bool Quest::getQuestObjectiveStatus(int QuestObjectiveID) {
+	// search for QuestObjectiveID
+	int QuestObjectivePos = -1;
+	for (size_t i = 0; i < QuestObjectivs.size(); i++) {
+		if (QuestObjectivs.at(i).QuestObjectiveID == QuestObjectiveID) {
+			QuestObjectivePos = i;
+			break;
+		}
+	}
+
+	// return if no QuestQobjective was found
+	if (QuestObjectivePos == -1) return false;
+
+	return QuestObjectivs.at(QuestObjectivePos).completed;
+}
--- Quest.h
+++ Quest.h
@@ -1,13 +1,31 @@
 #pragma once
 #ifndef __QUEST__
 #define __QUEST__
+#include "TextBox.h"
+#include "ItemClass.h"
+#include "Entety.h"
 
+struct QuestObjective {
+	bool completed = false;
+	std::wstring Description;
+	int QuestObjectiveID;
+};
 
 class Quest {
 public:
-	
+	Quest(std::vector<QuestObjective> QuestObjectivs);
+	void setQuestObjectiveStatus(int QuestObjectiveID, bool status);
+	bool getQuestObjectiveStatus(int QuestObjectiveID);
+	void drawQuest(int x, int y, Console* console);
+
 private:
-	
+	std::vector<QuestObjective> QuestObjectivs;
+	int DefultQuestDisplayBoxFormate[2] = {
+		TEXT_HEAD,
+		TEXT_END
+	};
+
+	TextBox* QuestDisplayTextBox = new TextBox(DefultQuestDisplayBoxFormate, 2);
 };
 
 #endif // __QUEST__
